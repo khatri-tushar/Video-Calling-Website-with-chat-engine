01b6a325b167dfed9a05c88bfc7c44adb4d909be
khatri-tushar <tushar.khatri.it19@nsut.ac.in>
2021-03-15 12:53:38 +0530

    added mute and turn video off functionality

--- public/script.js
+++ public/script.js
@@ -1,27 +1,139 @@
+
+
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
+})
+let myVideoStream
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  myVideoStream = stream  
+  addVideoStream(myVideo, stream)
 
-var video = document.getElementById('video');
-// Get access to the camera!
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+  })
 
-
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-
-    navigator.mediaDevices.getUserMedia({ 
-        video: true,
-        audio: true 
-    }).then(function(stream) {
-        //video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.play();
-    });
-}
-
-socket.emit('join-room', ROOM_ID);
-socket.on('user-connected',()=>{
-    connectToNewUser();
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
+  })
 })
 
-const connectToNewUser = ()=>{
-    console.log("new user")
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
 }
- 
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+
+let text = $('input')
+
+$('html').keydown((e)=>{
+    //on enter press
+    if(e.which == 13 && text.val().length!=0){
+        console.log(text.val())
+        socket.emit('message',text.val());
+        text.val('');
+    }
+})
+
+socket.on('createMessage',message =>{
+    $('.messages').append(`<li class="message"><b>user</b><br>${message}</li>`)
+    scrollToBottom();
+})
+
+const scrollToBottom = ()=>{
+    let d = $('.main_chat_window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
+const muteUnmute = ()=>{
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if(enabled){
+        setUnmuteButton();
+        myVideoStream.getAudioTracks()[0].enabled =false;
+    }
+    else{
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled =true;
+    }
+}
+
+const setMuteButton =()=>{
+    const html =`
+    <i class ="fas fa-microphone"></i>
+    <span>Mute</span>
+    `
+    document.querySelector('.main_mute_button').innerHTML = html;
+}
+
+const setUnmuteButton =()=>{
+    const html =`
+    <i class ="fas fa-microphone-slash"></i>
+    <span>Mute</span>
+    `
+    document.querySelector('.main_mute_button').innerHTML = html;
+}
+
+
+
+const playStop = ()=>{
+    const enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if(enabled){
+        setStopButton();
+        myVideoStream.getVideoTracks()[0].enabled =false;
+    }
+    else{
+        setStartButton();
+        myVideoStream.getVideoTracks()[0].enabled =true;
+    }
+}
+
+const setStartButton =()=>{
+    const html =`
+    <i class ="fas fa-video"></i>
+    <span>Video ON</span>
+    `
+    document.querySelector('.main_stop_button').innerHTML = html;
+}
+
+const setStopButton =()=>{
+    const html =`
+    <i class ="fas fa-video-slash"></i>
+    <span>Video Off</span></span>
+    `
+    document.querySelector('.main_stop_button').innerHTML = html;
+}
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-
 const express = require('express');
-const socket = require('socket.io');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
-app.use(express.static('public'));
-//uuid 
+//uuid
 const { v4: uuidv4 } = require('uuid');
 
+const io = require('socket.io')(server);
+
+
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
 
 
 app.get('/', function(req,res){
@@ -17,18 +17,20 @@ app.get('/', function(req,res){
 })
 
 app.get('/:room',(req,res)=>{
-    res.render('room',{roomId:req.params.room}) 
+    res.render('room',{roomId:req.params.room})
 })
 
 
 io.on('connection', socket => {
-    
-    socket.on('join-room', (roomId) => { 
+    socket.on('join-room', (roomId,userId) => {
         socket.join(roomId);
-        io.to(roomId).emit('user-connected')
-    }); // listen to the event
-  });
+        socket.broadcast.to(roomId).emit('user-connected', userId);
+        
+        //for messages
+        socket.on('message',message=>{
+            io.to(roomId).emit('createMessage',message)
+        })
+    });
+});
 
-
-server.listen(3030); 
-
+server.listen(3000)
