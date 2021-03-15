154bc5b66dfc8f25247448e8eba7d3d6f99dd48b
khatri-tushar <tushar.khatri.it19@nsut.ac.in>
2021-03-15 12:55:06 +0530

    changed port

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: '/',
-  port: '3001'
+  port: '443'
 })
 let myVideoStream
 const myVideo = document.createElement('video')
